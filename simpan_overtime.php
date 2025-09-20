862678d40716cc3f14220f3a11723118e3abbeb7
scgeast <scgeast@gmail.com>
2025-09-20 19:57:22 +0700

    Update simpan_overtime.php

--- simpan_overtime.php
+++ simpan_overtime.php
@@ -6,14 +6,17 @@ $jam_mulai = $_POST['jam_mulai'];
 $jam_selesai = $_POST['jam_selesai'];
 $keterangan = $_POST['keterangan'];
 
-$sql = "INSERT INTO lembur (id_absen, jam_mulai, jam_selesai, keterangan)
-        VALUES ('$id_absen', '$jam_mulai', '$jam_selesai', '$keterangan')";
+$mulai = strtotime($jam_mulai);
+$selesai = strtotime($jam_selesai);
+$durasi = round(($selesai - $mulai) / 3600, 2); // dalam jam
+
+$sql = "INSERT INTO lembur (id_absen, jam_mulai, jam_selesai, keterangan, durasi)
+        VALUES ('$id_absen', '$jam_mulai', '$jam_selesai', '$keterangan', '$durasi')";
 
 if (mysqli_query($conn, $sql)) {
-  echo "Data lembur berhasil disimpan.";
-  echo "<br><a href='absen_tabel.php'>Kembali ke Tabel Absensi</a>";
+  echo "Data lembur berhasil disimpan.<br>";
+  echo "<a href='overtime_tabel.php'>Lihat Rekap Lembur</a>";
 } else {
   echo "Gagal menyimpan data: " . mysqli_error($conn);
 }
 ?>
-
