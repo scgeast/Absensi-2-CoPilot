bddf7501329a6fe6ea64c0b709cf21211fa63cc6
scgeast <scgeast@gmail.com>
2025-09-20 19:56:21 +0700

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -6,13 +6,12 @@ $tanggal = $_POST['tanggal'];
 $jam_masuk = $_POST['jam_masuk'];
 $jam_keluar = $_POST['jam_keluar'];
 
-$sql = "INSERT INTO absen (nama, tanggal, jam_masuk, jam_keluar) VALUES ('$nama', '$tanggal', '$jam_masuk', '$jam_keluar')";
-$result = mysqli_query($conn, $sql);
+$sql = "INSERT INTO absensi (nama, tanggal, jam_masuk, jam_keluar)
+        VALUES ('$nama', '$tanggal', '$jam_masuk', '$jam_keluar')";
 
-if ($result) {
+if (mysqli_query($conn, $sql)) {
   header("Location: absen_tabel.php");
 } else {
   echo "Gagal menyimpan data: " . mysqli_error($conn);
 }
 ?>
-
